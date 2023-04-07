f87ac01bf14838493ab96c9bd1e69af8307125cf
Johan Baskovec <johan@johanbaskovec.fr>
2023-04-07 23:52:41 +0200

    Fix: kill all NPCs spawned by obstacles, not only hosts and spiders

--- src/main.lua
+++ src/main.lua
@@ -436,8 +436,9 @@ function destroyPoopsAndFires()
             local entity = entities:Get(i)
             -- TODO: verify that player could reach the fire
             if entity ~= nil then
+                npc = entity:ToNPC()
                 -- Remove mobs spawned by destroying pots and skulls
-                if entity.Type == EntityType.ENTITY_SPIDER or entity.Type == EntityType.ENTITY_HOST then
+                if npc ~= nil then
                     entity:Die()
                 end
                 if entity.Type == EntityType.ENTITY_FIREPLACE and (settings.destroyNormalFires and entity.Variant == 0) or
